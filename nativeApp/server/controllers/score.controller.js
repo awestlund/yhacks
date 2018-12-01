bf3b5fff7c93d859c7a174d2421033f43c3a6f44
Amber Westlund <awestlund@wisc.edu>
2018-12-01 11:21:31 -0500

    database connected to server smartcar

--- nativeApp/server/controllers/score.controller.js
+++ nativeApp/server/controllers/score.controller.js
@@ -1,21 +1,65 @@
-const Score = require('../models/score.model');
+const Smartcar = require('../models/smartcar.model');
+const smartcar = require('smartcar');
+const CLIENT_ID = '14d850b9-3652-4765-897e-ffed1716cc54';
+const CLIENT_SECRET = 'c431ca8e-352f-47a9-836b-e5ce98896b33';
+
+// 1. Create an instance of Smartcar's client.
+const client = new smartcar.AuthClient({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUri: 'http://localhost:8000/callback',
+    scope: ['read_vehicle_info', 'read_location', 'control_security'],
+    testMode: true
+});
 
 exports.get = (req, res, next)=>{
-    let q = Score.find();
-    q.exec((err, scores)=>{
-      if(err){
-        return res.status(500).send(err);
-      }
-      console.log("here"+ scores);
-      res.send(scores);
-    });
+    const authUrl = client.getAuthUrl({forcePrompt: true});
+    res.send(`
+      <h1>Hello, World!</h1>
+      <a href=${authUrl}>
+        <button>Connect Car</button>
+      </a>
+    `);
+
+    // let q = Smartcar.find();
+    // q.exec((err, Smartcars)=>{
+    //   if(err){
+    //     return res.status(500).send(err);
+    //   }
+    //   console.log("here"+ Smartcars);
+    //   res.send(Smartcars);
+    // });
 }
 
 exports.add = (req, res, next)=>{
-    let newscore = new Score({name:req.body.name, score:req.body.score});
+    const code = req.query.code;
+    const newuser = {};
+    client.exchangeCode(code)
+        .then(function(access) {
+        // get all vehicles
+        // Log the access token response
+        console.log(JSON.stringify(access, null, 2));
+        // get accessToken string
+        newuser.accessToken = access.accessToken;
+        console.log(newuser.accessToken);
 
-    newscore.save(err=>{
-        if(err) return res.status(500).send(err);
-        res.send('nice job kiddo');
-    })
+        return smartcar.getVehicleIds(newuser.accessToken);
+
+        })
+        .then(function(response) {
+            newuser.vehicleIDs= response.vehicles;
+            return smartcar.getUserId(newuser.accessToken);
+        })
+        .then(function(userID){
+            newuser.userID = userID;
+            let newSmartcar = new Smartcar(newuser);
+
+            newSmartcar.save(err=>{
+                if(err) return res.status(500).send(err);
+                res.send('nice job kiddo');
+
+            })
+            // res.send('nice job kiddo2');
+
+        });
 }
