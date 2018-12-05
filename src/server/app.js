bfaf1f5c9508c2617b112e7718a6d9d1a9a8157c
Amber Westlund <awestlund@wisc.edu>
2018-12-05 16:40:48 -0500

    updating db password, still cannot connect

--- src/server/app.js
+++ src/server/app.js
@@ -8,9 +8,15 @@ var indexRouter = require('./routes/index');
 var scoresRouter = require('./routes/smartcar');
 
 const mongoose = require('mongoose');
-const mongo_url = 'mongodb://usethistoedit:editme123@ds123444.mlab.com:23444/yhack2018_smartcar';
+const mongo_url = 'mongodb://ds123444.mlab.com:23444/yhack2018_smartcar';
 // const mongo_url = 'mongodb://localhost:27017/example';
-mongoose.connect(mongo_url, {useNewUrlParser:true});
+mongoose.connect(mongo_url, {
+  auth: {
+    user: 'usethistoedit',
+    password: 'sm@artc@ar123'
+  },
+  useNewUrlParser: true
+});
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('err', console.log.bind(console, 'mongo connection error'));
